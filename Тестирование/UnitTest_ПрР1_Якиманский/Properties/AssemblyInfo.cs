9b02b01f0aa1d2e59727ee1f536be9ab598a13e0
WeRRes0 <100131274+WeRRes0@users.noreply.github.com>
2023-02-28 00:34:00 +0300

    Add files via upload

---
+++ Тестирование/UnitTest_ПрР1_Якиманский/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_ПрР1_Якиманский")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_ПрР1_Якиманский")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f7040819-c053-47e8-b7c4-74dae5e08ab4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Тестирование/UnitTest_ПрР1_Якиманский/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ПрР1_Якиманский;
+
+namespace UnitTest_ПрР1_Якиманский
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            /*int[,] X = new int[3, 4] { { 2, 3, 0, 45 },
+                { 6, 7, 1, 12 },
+                { 2, 4, 9, 13 }
+            };
+
+            int[] experted = new int[1] { 4 };
+
+            int c = Matrix.;*/
+        }
+    }
+}
